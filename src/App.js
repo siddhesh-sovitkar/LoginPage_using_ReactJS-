b060945e01ba8edd2c234e637b354662e0534f23
siddhesh-sovitkar <58511972+siddhesh-sovitkar@users.noreply.github.com>
2020-04-09 20:11:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import HomeScreen from "/screens/HomeScreen";
+
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <HomeScreen />
+      </div>
+    );
+  }
+}
